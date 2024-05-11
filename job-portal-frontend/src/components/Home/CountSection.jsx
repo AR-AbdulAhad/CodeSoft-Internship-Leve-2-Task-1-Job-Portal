e15dc80203352a303f9c5638572a63b974c38afd
Abdul Ahad <abdulahad010274@gmail.com>
2024-05-11 10:55:48 +0500

    Update CountSection.jsx

--- job-portal-frontend/src/components/Home/CountSection.jsx
+++ job-portal-frontend/src/components/Home/CountSection.jsx
@@ -11,7 +11,7 @@ const CountSection = () => {
     }, []);
 
     const fetchCounts = () => {
-        fetch('https://job-portal-api.up.railway.app/api/v1/user/getusercounts')
+        fetch('https://job-portal-nodejs-api.vercel.app/api/v1/user/getusercounts')
             .then(response => response.json())
             .then(data => {
                 animateCount(setEmployersCount, data.employerCount);
@@ -19,14 +19,14 @@ const CountSection = () => {
             })
             .catch(error => console.error('Error fetching user counts:', error));
 
-        fetch('https://job-portal-api.up.railway.app/api/v1/job/getavailablejobscount')
+        fetch('https://job-portal-nodejs-api.vercel.app/api/v1/job/getavailablejobscount')
             .then(response => response.json())
             .then(data => {
                 animateCount(setAvailableJobsCount, data.availableJobsCount);
             })
             .catch(error => console.error('Error fetching available jobs count:', error));
 
-        fetch('https://job-portal-api.up.railway.app/api/v1/application/applicationcount')
+        fetch('https://job-portal-nodejs-api.vercel.app/api/v1/application/applicationcount')
             .then(response => response.json())
             .then(data => {
                 animateCount(setApplicationCount, data.applicationCount);
